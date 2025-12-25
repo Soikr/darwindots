b3f04187664ff0a3f4f84fe72ed81d4e0be65775
Soikr <Soikr@proton.me>
2025-12-24 22:22:12 -0800

    Install xcodes and replace dorion with vesktop

--- flake.nix
+++ flake.nix
@@ -3,51 +3,59 @@
 
   outputs = {
     self,
-    pkgs-darwin,
-    pkgs-nixos,
-    disko,
+    nixpkgs-darwin,
+    nixpkgs-nixos,
     nix-darwin,
-    brew,
-    hm,
+    nix-homebrew,
+    home-manager,
+    disko,
+    sops-nix,
   }: {
     darwinConfigurations."snowmalus" = nix-darwin.lib.darwinSystem {
       specialArgs = {inherit self;};
 
       modules = [
-        brew.darwinModules.nix-homebrew
-        hm.darwinModules.home-manager
+        nix-homebrew.darwinModules.nix-homebrew
+        home-manager.darwinModules.home-manager
 
         ./hosts/snowmalus
       ];
     };
-    nixosConfigurations."winterberry" = pkgs-nixos.lib.nixosSystem {
+    nixosConfigurations."winterberry" = nixpkgs-nixos.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./hosts/winterberry
         disko.nixosModules.disko
+        sops-nix.nixosModules.sops
+
+        ./hosts/winterberry
       ];
     };
   };
 
   inputs = {
-    pkgs-nixos.url = "github:NixOS/nixpkgs/nixos-25.11";
-    pkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
-
-    disko = {
-      url = "github:nix-community/disko/latest";
-      inputs.nixpkgs.follows = "pkgs-nixos";
-    };
+    nixpkgs-nixos.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
 
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
-      inputs.nixpkgs.follows = "pkgs-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
-    brew.url = "github:zhaofengli/nix-homebrew";
-
-    hm = {
+    home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "pkgs-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs-nixos";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs-nixos";
     };
   };
 }
